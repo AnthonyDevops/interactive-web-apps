d930be1b2eafee7e1a04bd0a3bd69b6f9db5d5a9
AnthonyDevops <anthonybeukes334@gmail.com>
2023-06-24 15:59:51 +0200

    Challenge IWA4

---
+++ IWA4.3/scripts.js
@@ -0,0 +1,39 @@
+const date = 2050
+let status = 'student'
+let count = 0
+
+if (date) {
+	console.log("January", 'New Year’s Day')
+	console.log("March", 'Human Rights Day')
+	let date = 'April'
+	console.log(date, 'Family Day')
+	console.log(date, 'Freedom Day')
+	count = count + 4
+    
+}
+
+	if (status = 'student') {
+	  console.log('June', 'Youth Day')
+		count = count + 1
+        
+    }
+
+{
+    console.log('August', 'Women’s Day')
+	console.log('September', 'Heritage Day')
+	let date = 'December'
+	console.log(date, 'Day of Reconciliation')
+	count = count + 3
+    
+
+    }
+	 if (status = "parent") {
+	  console.log(date, 'Christmas Day');
+      console.log(date, "Day of Goodwill");
+		count = count + 1
+        
+    }
+
+	console.log("The year is:", date);
+    console.log('Your status is:', status)
+    console.log('The total holidays is:', count)
