882ebe00a8ab3b953e838226f02fb7091c38c958
AnthonyDevops <anthonybeukes334@gmail.com>
2023-07-06 00:50:44 +0200

    IWA6-Challenge2

---
+++ IWA0.6 Challenge 1/challenge2.js
@@ -0,0 +1,19 @@
+const rent = 400;
+const tax = '8%';
+const food = 51.7501;
+const salary = 800;
+const transport = 10.2;
+const hourOfDay = "0"
+const minuteOfDay = "0"
+
+// Only change below this line
+let balance;
+
+if (hourOfDay !== null && minuteOfDay !== null) {
+  const taxAsDecimal = parseInt(tax,10) / 100;
+  const startingAfterTax = salary * (1 - taxAsDecimal);
+  balance = startingAfterTax - transport - food - rent;
+}
+
+console.log('R'+balance.toFixed(2));
+// R274,05
